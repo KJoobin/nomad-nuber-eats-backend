70568530fbb1d1365569564c11e09fe8143bfff3
kjoobin <kjoobin>
2022-03-16 21:24:30 +0900

    add restaurants service

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,17 @@ import { Restaurant } from './restaurants/entities/restaurant.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: true,
+      entities: [Restaurant],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.test.env',
@@ -22,19 +33,6 @@ import { Restaurant } from './restaurants/entities/restaurant.entity';
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
       }),
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      synchronize: process.env.NODE_ENV !== 'prod',
-      logging: true,
-      entities: [Restaurant],
-      // migrations: ['src/migration/**/*.ts'],
-      // subscribers: ['src/subscriber/**/*.ts'],
     }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,14 +1,16 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { RestaurantsService } from './restaurants.service';
 
 @Resolver(() => Restaurant)
 export class RestaurantsResolver {
-  @Query(() => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
-  }
+  constructor(private readonly restaurantsService: RestaurantsService) {}
 
+  @Query(() => [Restaurant])
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantsService.getAll();
+  }
   @Mutation(() => Boolean)
   createRestaurant(
     @Args() createRestaurantInput: CreateRestaurantDto,
